5a6909c0eec6b474940b63b67471a2ebef9f907e
北极星 <75174838+52beijixing@users.noreply.github.com>
2024-09-02 19:40:56 +0800

    Update getInfo.py
    将导致出问题的双引号改为单引号，避免双引号嵌套

--- utils/getInfo.py
+++ utils/getInfo.py
@@ -561,7 +561,7 @@ def get_default_infos(default_dir: str, user_data: str, app_id: str):
                 for i in range(len(datas)):
                     data = datas[i]
                     if not data["title"] in contained_title:
-                        print(f"第{i}项目出现问题\ntype:{data["resource_type_code"]}暂未支持\n请将控制台信息和链接复制并前往 https://github.com/52beijixing/smartedu-download/issues 反馈！")
+                        print(f'第{i}项目出现问题\ntype:{data["resource_type_code"]}暂未支持\n请将控制台信息和链接复制并前往 https://github.com/52beijixing/smartedu-download/issues 反馈！')
                         state = False
                 if not state:
                     raise
